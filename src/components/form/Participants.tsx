b402cad7547829d0258a654f27cec06b66a2d924
2244761 <2244761@slu.edu.ph>
2025-10-07 22:09:23 +0800

    Add form component and api helper

--- src/components/form/Participants.tsx
+++ src/components/form/Participants.tsx
@@ -1,5 +1,5 @@
 import { MantineProvider, TagsInput, TextInput } from "@mantine/core";
-import React, { useState } from "react";
+import { useState } from "react";
 import { FaPlusSquare } from "react-icons/fa";
 
 const Participants = () => {
---
+++ src/providers/auth-provider.ts
@@ -0,0 +1,53 @@
+import type { AuthProvider } from "@refinedev/core";
+import supabaseClient from "../config/supabaseClient";
+
+export const authProvider: AuthProvider = {
+  check: async () => {
+    const {
+      data: { session },
+    } = await supabaseClient.auth.getSession();
+
+    if (!session) return { authenticated: false, redirectTo: "/login" };
+
+    return { authenticated: true, redirectTo: "/" };
+  },
+  login: async () => {
+    const { error } = await supabaseClient.auth.signInWithOAuth({
+      provider: "google",
+    });
+
+    if (error)
+      return {
+        success: false,
+        error,
+      };
+
+    return { success: true, redirectTo: "/" };
+  },
+  logout: async () => {
+    const { error } = await supabaseClient.auth.signOut();
+
+    if (error)
+      return {
+        success: false,
+        error,
+      };
+
+    return { success: true, redirectTo: "/login" };
+  },
+  getIdentity: async () => {
+    const { data, error } = await supabaseClient.auth.getUser();
+
+    if (error) console.error("Error getting user:", error.message);
+
+    if (data?.user) {
+      return data;
+    }
+
+    return null;
+  },
+  onError: async (error) => {
+    console.error(error);
+    return { error };
+  },
+};

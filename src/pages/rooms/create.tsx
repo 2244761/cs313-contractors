104101db50d56479335168ec89d82997ef7d8f02
JAZREIL JARON SABOG <2244761@slu.edu.ph>
2025-11-20 19:48:30 +0800

    Added initial submission of room images

--- src/pages/rooms/create.tsx
+++ src/pages/rooms/create.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { Room } from "../../utils/types";
-import { useCreate } from "@refinedev/core";
+import { useCreate, useList } from "@refinedev/core";
 import {
   MantineProvider,
   NumberInput,
@@ -8,30 +8,38 @@ import {
   Textarea,
   TextInput,
 } from "@mantine/core";
-import { Dropzone, IMAGE_MIME_TYPE } from "@mantine/dropzone";
+import {
+  Dropzone,
+  IMAGE_MIME_TYPE,
+  type FileWithPath,
+} from "@mantine/dropzone";
 import supabase from "../../config/supabaseClient";
+import {
+  MdAddPhotoAlternate,
+  MdErrorOutline,
+  MdOutlineFileUpload,
+} from "react-icons/md";
+import { FaXmark } from "react-icons/fa6";
 
 export const RoomCreate = () => {
-  const [room, setRoom] = useState<Room>({
-    id: 0,
-    name: "",
-    room: "",
-    status: "Available",
-    description: "",
-    images: [],
-    capacity: 0,
-  });
+  const [files, setFiles] = useState<FileWithPath[]>([]);
+  const { query } = useList<Room>();
 
-  const {
-    mutate,
-    mutation: { isPending: isCreating },
-  } = useCreate();
+  const totalRoom = query.data?.data.length;
+  // const [room, setRoom] = useState<Room>({
+  //   id: 0,
+  //   name: "",
+  //   room: "",
+  //   status: "Available",
+  //   description: "",
+  //   images: [],
+  //   capacity: 0,
+  // });
 
-  // const { data } = supabase.storage
-  //   .from("room_thumbnails")
-  //   .getPublicUrl("1/thumbnail-1.jpg");
-
-  // console.log(data);
+  // const {
+  //   mutate,
+  //   mutation: { isPending: isCreating },
+  // } = useCreate();
 
   // const handleUpdate = async () => {
   //   await mutate({
@@ -46,14 +54,118 @@ export const RoomCreate = () => {
   //   });
   // };
 
+  // const storeThumbnails = files.map((file, index) => {
+  //   const blobUrl = URL.createObjectURL(file);
+  //   const imageName = `thumbnail-${index + 1}`;
+  //   return {
+  //     file,
+  //     blobUrl,
+  //     name: imageName,
+  //   };
+  // });
+
+  const removePreview = (name: string) => {
+    setFiles((files) => files.filter((file) => file.name !== name));
+  };
+
+  console.log(files.map((f) => f.name));
+
+  const handleUpload = async (e: React.FormEvent) => {
+    e.preventDefault();
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+
+      const { data, error } = await supabase.storage
+        .from("room_thumbnails")
+        .upload(`1/${file.name}`, file);
+
+      if (error) console.log(error);
+
+      console.log(data);
+    }
+  };
+
+  const previewThumbnails = files.map((file, index) => {
+    const blobUrl = URL.createObjectURL(file);
+    // const imageName = `thumbnail-${index + 1}`;
+
+    if (IMAGE_MIME_TYPE.includes(file.type as any)) {
+      return (
+        <>
+          <div key={index} className="relative">
+            <img
+              src={blobUrl}
+              onLoad={() => URL.revokeObjectURL(blobUrl)}
+              className="w-56 h-56 object-cover"
+            />
+            <button
+              type="button"
+              className="absolute right-0 top-0 cursor-pointer"
+              onClick={() => removePreview(file.name)}
+            >
+              <FaXmark />
+            </button>
+          </div>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <div className="relative">
+            <video
+              className="w-56 h-56 object-cover"
+              controls
+              key={index}
+              onLoad={() => URL.revokeObjectURL(blobUrl)}
+            >
+              <source src={blobUrl} type="video/mp4" />
+            </video>
+            <button
+              type="button"
+              className="absolute right-0 top-0 cursor-pointer"
+              onClick={() => removePreview(file.name)}
+            >
+              <FaXmark />
+            </button>
+          </div>
+        </>
+      );
+    }
+  });
+
   return (
     <MantineProvider>
       <div className="flex justify-center items-center">
         <form
-          // onSubmit={}
+          onSubmit={handleUpload}
           className="w-2xl h-max bg-white rounded-xl p-8 border border-gray-200 flex flex-col gap-4"
         >
-          <div>
+          <Dropzone
+            onDrop={(file) => setFiles((prev) => [...prev, ...file])}
+            accept={{ "image/*": [], "video/mp4": [] }}
+          >
+            <div className="flex justify-center items-center gap-4 border rounded border-dashed cursor-pointer hover:bg-gray-100 transition-colors duration-200 p-4">
+              <Dropzone.Accept>
+                <MdOutlineFileUpload size={52} />
+              </Dropzone.Accept>
+              <Dropzone.Reject>
+                <MdErrorOutline size={52} />
+              </Dropzone.Reject>
+              <Dropzone.Idle>
+                <MdAddPhotoAlternate size={52} />
+              </Dropzone.Idle>
+
+              <div>
+                <h1>Drag images here or click to select files</h1>
+                <p>
+                  Attach as many files as you like, each file should not exceed
+                  5mb
+                </p>
+              </div>
+            </div>
+          </Dropzone>
+          <div className="flex gap-2">{previewThumbnails}</div>
+          {/* <div>
             <TextInput label="Facility" />
           </div>
           <div>
@@ -67,7 +179,7 @@ export const RoomCreate = () => {
             <div>
               <Select data={["Available", "Unavailable"]} label="Status" />
             </div>
-          </div>
+          </div> */}
           <button
             type="submit"
             className="p-3 bg-[var(--primary)] cursor-pointer text-white rounded"
